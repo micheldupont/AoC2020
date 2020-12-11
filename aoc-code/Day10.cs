dc733fae8be2a1e38f1d93438dd7f364a9a038c0
Mike <micheld@gmail.com>
2020-12-10 21:07:10 -0500

    day 10

---
+++ aoc-code/Day10.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace aoc_code
+{
+	public class Day10
+	{
+		public int Run(string input)
+		{
+			var diffOf1Count = 0;
+			var diffOf3Count = 1;
+
+			var adapters = input.Split("\r\n").Select(int.Parse).OrderBy(a => a).ToArray();
+
+			for (var i = 0; i < adapters.Length; i++)
+			{
+				var prev = i == 0 ? 0 : adapters[i - 1];
+				var curr = adapters[i];
+
+				var diff = curr - prev;
+				if (diff == 1)
+				{
+					diffOf1Count += 1;
+				}
+
+				if (diff == 3)
+				{
+					diffOf3Count += 1;
+				}
+			}
+
+			return diffOf1Count * diffOf3Count;
+		}
+
+		public ulong Run2(string input)
+		{
+			var adapters = input.Split("\r\n").Select(int.Parse).OrderBy(a => a).ToArray();
+			adapters = new[] {0}.Concat(adapters).Concat(new[] {adapters.Last() + 3}).ToArray();
+
+			var adaptersBunch = SplitInBunches(adapters);
+
+			var total = adaptersBunch.Select(CalculatePermutations).Aggregate((a, b) => a * b);
+
+			return total;
+		}
+
+		private static List<int[]> SplitInBunches(int[] adapters)
+		{
+			var adaptersBunch = new List<int[]>();
+
+			var bunchIndex = 0;
+			for (var i = 1; i < adapters.Length; i++)
+			{
+				var prev = adapters[i - 1];
+				var curr = adapters[i];
+
+				var diff = curr - prev;
+				if (diff == 3)
+				{
+					adaptersBunch.Add(adapters.Skip(bunchIndex).Take(i - bunchIndex).ToArray());
+					bunchIndex = i;
+				}
+			}
+
+			return adaptersBunch;
+		}
+
+		public ulong CalculatePermutations(int[] values)
+		{
+			switch (values.Length)
+			{
+				case 1:
+				case 2:
+					return 1;
+				case 3:
+					return 2;
+				case 4:
+					return 4;
+				case 5:
+					return 7;
+				default:
+					throw new Exception("Unexpected");
+			}
+		}
+	}
+}
---
+++ aoc-tests/Day10Tests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using aoc_code;
+using aoc_tests.TestInputs;
+using Xunit;
+
+namespace aoc_tests
+{
+	public class Day10Tests
+	{
+		[Fact]
+		public void Run1ExampleA()
+		{
+			var input = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+			var test = new Day10().Run(input);
+
+			Assert.Equal(7 * 5, test);
+		}
+
+		[Fact]
+		public void Run1ExampleB()
+		{
+			var input = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+			var test = new Day10().Run(input);
+
+			Assert.Equal(22 * 10, test);
+		}
+
+		[Fact]
+		public void Run1Mine()
+		{
+			var test = new Day10().Run(TestInput.Day10);
+
+			Assert.Equal(2380, test);
+		}
+
+		[Fact]
+		public void RunPermutationTestA()
+		{
+			var test = new Day10().CalculatePermutations(new[] {1, 2});
+
+			Assert.Equal((ulong) 1, test);
+		}
+
+		[Fact]
+		public void RunPermutationTestB()
+		{
+			var test = new Day10().CalculatePermutations(new[] {1, 2, 3});
+
+			Assert.Equal((ulong) 2, test);
+		}
+
+		[Fact]
+		public void RunPermutationTestC()
+		{
+			var test = new Day10().CalculatePermutations(new[] {1, 2, 3, 4});
+
+			Assert.Equal((ulong) 4, test);
+		}
+
+		[Fact]
+		public void RunPermutationTestD()
+		{
+			var test = new Day10().CalculatePermutations(new[] {45, 46, 47, 48, 49});
+
+			Assert.Equal((ulong) 7, test);
+		}
+
+		[Fact]
+		public void Run2ExampleA()
+		{
+			var input = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+			var test = new Day10().Run2(input);
+
+			Assert.Equal((ulong) 8, test);
+		}
+
+		[Fact]
+		public void Run2ExampleB()
+		{
+			var input = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+			var test = new Day10().Run2(input);
+
+			Assert.Equal((ulong) 19208, test);
+		}
+
+		[Fact]
+		public void Run2Mine()
+		{
+			var test = new Day10().Run2(TestInput.Day10);
+
+			Assert.Equal((ulong) 48358655787008, test);
+		}
+	}
+}

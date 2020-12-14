592f4e38ff40f431a2978654d576629e19c8cee9
Mike <micheld@gmail.com>
2020-12-13 20:35:09 -0500

    day 13

---
+++ aoc-code/Day13.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace aoc_code
+{
+	public class Day13
+	{
+		public int Run(string input)
+		{
+			var lines = input.Split("\r\n");
+			var startTime = int.Parse(lines[0]);
+
+			var buses = lines[1].Split(",").Where(c => c != "x").Select(int.Parse).ToList();
+
+			var checkTime = startTime;
+
+			while (true)
+			{
+				var foundBus = buses.Find(b => checkTime % b == 0);
+
+				if (foundBus != 0)
+				{
+					var timeToWait = checkTime - startTime;
+
+					return foundBus * timeToWait;
+				}
+
+				checkTime += 1;
+			}
+		}
+
+		public ulong Run2(string input)
+		{
+			var lines = input.Split("\r\n");
+			var buses = lines[1].Split(",").Select(c => c == "x" ? null : (ulong?) int.Parse(c)).ToList();
+
+
+			var busesWithCheckTimeOffset = Enumerable.Range(0, buses.Count).Select(i => (ulong) i)
+				.Zip(buses, (offset, bus) => new Tuple<ulong?, ulong>(bus, offset)).Where(t => t.Item1.HasValue)
+				.OrderBy(bwo => bwo.Item1)
+				.ToList();
+
+			ulong time;
+			if (busesWithCheckTimeOffset[0].Item2 > busesWithCheckTimeOffset[0].Item1.Value
+			) // cannot start with negative time... especially when type is unsigned...
+			{
+				var ratio = busesWithCheckTimeOffset[0].Item2 / busesWithCheckTimeOffset[0].Item1.Value;
+				time = busesWithCheckTimeOffset[0].Item1.Value * (ratio + 1) - busesWithCheckTimeOffset[0].Item2;
+			}
+			else
+			{
+				time = busesWithCheckTimeOffset[0].Item1.Value - busesWithCheckTimeOffset[0].Item2;
+			}
+
+
+			var step = busesWithCheckTimeOffset[0].Item1.Value;
+			var index = 0;
+
+			while (index < busesWithCheckTimeOffset.Count - 1)
+			{
+				var next = busesWithCheckTimeOffset[index + 1];
+
+				var synced = false;
+				while (!synced)
+				{
+					var nextTime = next.Item2 + time;
+
+					if (nextTime % next.Item1.Value == 0)
+					{
+						step = step * next.Item1.Value;
+						index += 1;
+						synced = true;
+					}
+					else
+					{
+						time += step;
+					}
+				}
+			}
+
+			return time;
+		}
+	}
+}
---
+++ aoc-tests/Day13Tests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using aoc_code;
+using aoc_tests.TestInputs;
+using Xunit;
+
+namespace aoc_tests
+{
+	public class Day13Tests
+	{
+		[Fact]
+		public void Run1ExampleA()
+		{
+			var input = @"939
+7,13,x,x,59,x,31,19";
+
+			var test = new Day13().Run(input);
+
+			Assert.Equal(59 * 5, test);
+		}
+
+		[Fact]
+		public void Run1Mine()
+		{
+			var test = new Day13().Run(TestInput.Day13);
+
+			Assert.Equal(1835, test);
+		}
+
+		[Fact]
+		public void Run2ExampleA()
+		{
+			var input = @"939
+7,13,x,x,59,x,31,19";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 1068781, test);
+		}
+
+		[Fact]
+		public void Run2ExampleB()
+		{
+			var input = @"939
+17,x,13,19";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 3417, test);
+		}
+
+		[Fact]
+		public void Run2ExampleC()
+		{
+			var input = @"939
+67,7,59,61";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 754018, test);
+		}
+
+		[Fact]
+		public void Run2ExampleD()
+		{
+			var input = @"939
+67,x,7,59,61";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 779210, test);
+		}
+
+		[Fact]
+		public void Run2ExampleE()
+		{
+			var input = @"939
+67,7,x,59,61";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 1261476, test);
+		}
+
+		[Fact]
+		public void Run2ExampleF()
+		{
+			var input = @"939
+1789,37,47,1889";
+
+			var test = new Day13().Run2(input);
+
+			Assert.Equal((ulong) 1202161486, test);
+		}
+
+		[Fact]
+		public void Run2Mine()
+		{
+			var test = new Day13().Run2(TestInput.Day13);
+
+			Assert.Equal((ulong) 247086664214628, test);
+		}
+	}
+}
